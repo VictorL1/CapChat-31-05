4edc5c686e3a02661175a3936cdfb0696dc02511
VictorL1 <victor.ltalamon@gmail.com>
2022-06-13 23:45:06 +0200

    Créer un jeu

--- index.js
+++ index.js
@@ -42,6 +42,33 @@ window.Inscription = function() {
     )
 }
 
+window.SetJeu = function () {
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+    const name = document.getElementById("nomJeu").value;
+    // const indice = document.getElementById("clueJeu").value;
+    // const singular = document.getElementById("img_singular").value;
+    // const neutral = document.getElementById("img_neutral").value;
+    const theme = document.getElementById("listetheme").value;
+    console.log(name);
+    axios.post('http://localhost:3000/nouveau-jeu', {
+        "NomJeu": name,
+        "theme": theme,
+        // "indice": indice,
+        // "ImgSingular" : singular,
+        // "ImgNeutral" : neutral
+     })
+        .then(
+            () => {
+                console.log("tout va bien");
+                alert("Le jeu " + name + " a été créé, vous pouvez le remplir !")
+            }
+        ).catch(
+            (err) => {
+                console.log(err);
+            }
+        );
+}
+
 window.Deconnexion = function() {
     
         localStorage.clear();
@@ -49,3 +76,74 @@ window.Deconnexion = function() {
     
 
 }
+
+window.loadListe = function() {
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+    axios.get('http://localhost:3000/theme')
+    .then(
+        data => {
+            console.log(data.data);
+            let string = "";
+            for (let i = 0; i < data.data.length; i++) {
+                string += `<option value="${data.data[i].IDtheme}">${data.data[i].NomTheme}</option>`
+            }
+            document.getElementById("listetheme").innerHTML = string;
+        }
+    )
+    .catch(
+        err => {
+            console.error(err);
+        }
+    )
+
+}
+
+window.loadJeu = function() {
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+    axios.get('http://localhost:3000/jeu')
+    .then(
+        data => {
+            console.log(data.data);
+            let string = "";
+            for (let i = 0; i < data.data.length; i++) {
+                string += `<div class="col"><a type="submit" class="btn btn-outline-primary" href="createjeu.html?IDJeu=${data.data[i].IDJeu}" value="${data.data[i].IdJeu}">${data.data[i].NomJeu}</a></div>`
+            }
+            document.getElementById("listejeu").innerHTML = string;
+        }
+    )
+    .catch(
+        err => {
+            console.error(err);
+        }
+    )
+
+}
+
+window.jeuARemplir = function() {
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const IDJeu = urlParams.get('IDJeu')
+    axios.get('http://localhost:3000/jeu')
+    .then(
+        data => {
+            console.log(data.data);
+            let string = "";
+            for (let i = 0; i < data.data.length; i++) {
+                string += `<div class="col"><a type="submit" class="btn btn-outline-primary" href="createjeu.html?IDJeu=${data.data[i].IDJeu}" value="${data.data[i].IdJeu}">${data.data[i].NomJeu}</a></div>`
+            }
+            document.getElementById("listejeu").innerHTML = string;
+        }
+    )
+    .catch(
+        err => {
+            console.error(err);
+        }
+    )
+}
+
+    window.jeu = function() {
+        loadListe();
+        loadJeu();
+        }
+    
